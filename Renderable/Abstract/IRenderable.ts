18f06a72f03829e5447634eed6ea1766ae24f638
Antonio <antoniorivera.cs18@gmail.com>
2023-07-09 11:16:06 -0400

    Added different rendering object possibilities.

--- Renderable/Abstract/IRenderable.ts
+++ Renderable/Abstract/IRenderable.ts
@@ -1,3 +1,15 @@
+import { Point } from "../../Point/Point";
+import { Vector } from "../../Vector/Vector";
+
 export interface IRenderable {
   Draw(): void;
+}
+
+export interface IMoveable {
+  GetPos(): Vector;
+}
+
+export interface IRenderableMovable {
+  Draw(): void;
+  GetPos(): Vector;
 }
