909003f7cabacbf1a922fe3e2e863434809e2ef5
Antonio <antoniorivera.cs18@gmail.com>
2023-07-13 10:31:27 -0400

    Renderable needs to know how to draw according to grid.

--- Renderable/Abstract/IRenderable.ts
+++ Renderable/Abstract/IRenderable.ts
@@ -1,3 +1,4 @@
+import { IGrid } from "../../Grid/Abstract/IGrid";
 import { Point } from "../../Point/Point";
 import { Vector } from "../../Vector/Vector";
 
@@ -10,6 +11,7 @@ export interface IMoveable {
 }
 
 export interface IRenderableMovable {
+  GetCurrentGrid(): IGrid;
   Draw(): void;
   GetPos(): Vector;
 }
