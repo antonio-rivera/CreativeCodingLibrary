d8a057fd658ac2ed5e99da76215f9096511e2eba
Antonio <antoniorivera.cs18@gmail.com>
2023-07-15 07:32:36 -0400

    IGrid controls scaling to itself

--- Grid/Abstract/IGrid.ts
+++ Grid/Abstract/IGrid.ts
@@ -1,6 +1,13 @@
+import { Point } from "../../Point/Point";
+import { Vector } from "../../Vector/Vector";
+
 export interface IGrid {
   Draw(): void;
+  GetOrigin(): Point;
   GetWidth(): number;
   GetHeight(): number;
   GetCellSize(): number;
+  TranslateToDraw(position: Vector): [x: number, y: number];
+  ScaleVecToGrid(vec: Vector): Vector;
+  ScaleToGrid(pixels: number): number;
 }
