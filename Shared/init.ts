970faf3e33a818cdb78865eeef7c1912bd8e1343
Antonio <antoniorivera.cs18@gmail.com>
2023-07-16 11:59:37 -0400

    Testing with shared ctx and grid variables approach.

---
+++ Shared/init.ts
@@ -0,0 +1,6 @@
+import { CartesianGrid } from "../../Library/Grid/Concrete/CartesianGrid";
+export const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+export const ctx = canvas.getContext("2d")!;
+export const grid = new CartesianGrid(50, canvas.width, canvas.height, ctx);
