2e5618973e835ab456090dd80f1c1656a8079092
Antonio <antoniorivera.cs18@gmail.com>
2023-07-26 21:21:56 -0400

    Had to fix a lot of incorrect calculations

--- Animation/VerletObject.ts
+++ Animation/VerletObject.ts
@@ -6,19 +6,19 @@ export abstract class VerletObject {
 
   constructor(c_pos: Vector) {
     this.acceleration = new Vector(0, 0);
-    this.current_pos.Assign(c_pos);
-    this.old_pos.Assign(c_pos);
+    this.current_pos = Vector.fromPoint(c_pos.point);
+    this.old_pos = Vector.fromPoint(c_pos.point);
   }
 
-  public UpdatePosition(dt: number): void {
+  public UpdatePosition(dt: number, deltaTimeMultiplier: number): void {
     const velocity = this.current_pos.Sub(this.old_pos);
 
     this.old_pos = Vector.fromPoint(this.current_pos.point);
-
     this.current_pos = this.current_pos
       .Add(velocity)
-      .Add(this.acceleration!)
-      .Scale(dt * dt);
+      .Add(this.acceleration.Scale(dt * dt));
+
+    this.acceleration = new Vector(0, 0);
   }
 
   public Accelerate(acc: Vector): void {
